96f8e4ce4da7037169d36af215cbaba7416df1ed
marie-jakob <mjakob@cs.uni-freiburg.de>
2021-05-03 13:12:24 +0200

    Update demographic questions.

--- experiment-files/demographics.js
+++ experiment-files/demographics.js
@@ -7,13 +7,32 @@ Adapted from Hannah Dames.
 let question_age = {
     type: "survey-text",
     questions: [
-        {prompt: "<div>Im Folgenden bitten wir Sie um einige persönliche Angaben.</div>" +
-                "<br>" + "<br>" + "<br>" +
+        {
+            prompt: "<div id='Header'>Demographische Angaben</div>" +
+                "<div style='text-align: left;'>Im Folgenden bitten wir Sie um einige persönliche Angaben.<br><br>" +
                 "Wie alt sind Sie? ",
             rows: 1, columns: 20,
             name: "age",
             required: true
-        }
+        },
+        {
+            prompt: "<div style='text-align: left;'>Was ist Ihre Muttersprache? ",
+            rows: 1, columns: 20,
+            name: "native",
+            required: true
+        },
+        {
+            prompt: "<div style='text-align: left;'>Was ist Ihr Geschlecht? ",
+            rows: 1, columns: 20,
+            name: "gender",
+            required: false
+        },
+        {
+            prompt: "<div style='text-align: left;'>Falls Sie aktuell studieren: <br> Welches Fach studieren Sie? ",
+            rows: 1, columns: 20,
+            name: "subject",
+            required: false
+        },
     ],
     button_label: "Weiter"
 }
@@ -26,21 +45,19 @@ let scale_gender = [
     "keine Angabe"
 ];
 
-let scale_school = ["kein Schulabschluss",
-    "Haupt-/Volksschulabschluss",
-    "Mittlere Reife/Realschule/Abschluss der Polytechnischen Oberschule",
-    "Fachhochschulreife",
-    "Abitur",
-    "Sonstiges"
-]
-
 let scale_education = ["keine Berufsausbildung",
     "noch in Ausbildung oder Student/in (noch kein Abschluss)",
     "abgeschlossene Lehre oder Meisterbrief",
-    "Berufsakademie-, Fachhochschul- oder Universitätsabschluss",
+    "Bachelorabschluss",
+    "Masterabschluss",
     "Promotion oder höher",
     "sonstiger beruflicher Ausbildungsstand"
-]
+];
+
+let scale_yes_no = [
+    "Ja",
+    "Nein"
+];
 
 let question_demographic_choice = {
     type: 'survey-multi-choice',
@@ -49,18 +66,20 @@ let question_demographic_choice = {
             options: scale_gender,
             name: "gender",
             required: true},
-        {prompt: "<strong>Was ist der höchste Schulabschluss, den Sie besitzen?</strong>",
-            options: scale_school,
-            name: "school",
-            required: true},
         {prompt: "<strong>Was ist Ihr höchster beruflicher Ausbildungsstand?</strong>",
             options: scale_education,
             name: "education",
             required: true},
+        {prompt: "<strong>Befinden Sie sich aktuell in Ausbildung oder studieren Sie?</strong>",
+            options: scale_yes_no,
+            name: "training",
+            required: true},
     ],
     button_label: "Weiter",
     on_finish: function(data) {
-        let resp_tmp = jsPsych.data.get().values()[1]["response"]["education"];
+        let data_tmp = jsPsych.data.get().values();
+        let resp_tmp = data_tmp[data_tmp.length - 1]["response"]["education"];
+        console.log("resp tmp: ", resp_tmp);
         if (resp_tmp === 'noch in Ausbildung oder Student/in (noch kein Abschluss)') {
             education_text = true;
         } else {
@@ -73,11 +92,12 @@ let question_demographic_choice = {
 let question_demographic_text = {
     type: "survey-text",
     questions: [
-        {prompt: '<div class="Instruction"> Sie haben angegeben, dass Sie sich noch in der Ausbildung befinden oder ' +
+        {prompt: '<div style="text-align: left"> Sie haben angegeben, dass Sie sich noch in der Ausbildung befinden oder ' +
                 'studieren. </div>' +
-                '<div class="Instruction"> Welche Ausbildung absolvieren Sie aktuell bzw. welches ' +
+                '<div style="margin-bottom: 25px; text-align: left">' +
+                'Welche Ausbildung absolvieren Sie aktuell bzw. welches ' +
                 'Studienfach studieren Sie?</div>',
-            rows: 10, columns: 80,
+            rows: 1, columns: 50,
             name: "education_text",
             required: true},
     ],
@@ -96,8 +116,8 @@ let conditional_education = {
 let demographics = {
     timeline: [
         question_age,
-        question_demographic_choice,
-        conditional_education
+        //question_demographic_choice,
+        //conditional_education
     ],
     on_start: function() { EXP_PART = 'demographics'}
 }
