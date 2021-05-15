2c9ce93508e7bb33df4f5c06ff68b05a6ed45054
marie-jakob <mjakob@cs.uni-freiburg.de>
2021-05-15 15:48:47 +0200

    Add post-experimental questions

--- experiment-files/post-exp-questions.js
+++ experiment-files/post-exp-questions.js
@@ -4,27 +4,45 @@ the instructions, etc.)
 Adapted from Hannah Dames.
  */
 
+
+let questions_understanding = {
+    type: 'survey-text',
+    questions: [
+        {
+            prompt: "<p id='Header'>Fragen zur Teilnahme</p>" +
+                "<p style='text-align: left;'>Beschreiben Sie bitte, was eine „Typ A“ Erinnerung ist, " +
+                "bzw. in welchen Fällen Sie vorhin „A“ geantwortet haben.</p>",
+            required: true,
+            rows: 3,
+            columns: 70,
+            custom_warning: "Bitte geben Sie eine Antwort ein."
+        },
+        {
+            prompt: "<p style='text-align: left;'>Beschreiben Sie bitte außerdem, was eine „Typ B“ Erinnerung ist, " +
+                "bzw. in welchen Fällen Sie vorhin „B“ geantwortet haben.</p>",
+            required: true,
+            rows: 3,
+            columns: 70,
+            custom_warning: "Bitte geben Sie eine Antwort ein."
+        },
+        {
+            prompt: "<p style='text-align: center;'>Was haben Sie danach mit dem Schieberegler angegeben?</p>",
+            required: true,
+            rows: 3,
+            columns: 70,
+            custom_warning: "Bitte geben Sie eine Antwort ein."
+        },
+
+    ],
+    button_label: "Weiter",
+}
+
+
 let scale_yesno = [
     "Ja",
     "Nein"
 ];
 
-
-let question_disturbed = {
-    type: 'survey-multi-choice',
-    questions: [{
-        prompt: "<p>Konnten Sie das Experiment ohne Störungen durchführen?</p>",
-        options: scale_yesno,
-        name: "post_disturbed",
-        required: true
-    }
-    ],
-    button_label: "Continue",
-    on_start: function() {
-        EXP_PART = "post-exp-questions";
-        $('body').css('cursor', 'default');
-    }
-};
 
 
 let scale_serious = [
@@ -37,7 +55,8 @@ let questions_data = {
     type: 'survey-multi-choice',
     questions: [
         {
-            prompt: "<strong>Konnten Sie das Experiment ohne Störungen durchführen?</strong>",
+            prompt: "<p id='Header'>Fragen zur Teilnahme</p>" +
+                "<strong>Konnten Sie das Experiment ohne Störungen durchführen?</strong>",
             options: scale_yesno,
             name: "post_disturbed",
             required: true
@@ -70,6 +89,7 @@ let question_comments = {
 
 let post_exp_questions = {
     timeline: [
+        questions_understanding,
         questions_data,
         question_comments
     ]
